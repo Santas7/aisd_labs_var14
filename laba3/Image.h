f8fd830c62704a5bfe2b34c736e8a179866be390
Andrey_Panyshkin <86359412+Santas7@users.noreply.github.com>
2022-12-11 13:51:50 +0400

    Add files via upload

---
+++ laba3/Image.h
@@ -0,0 +1,142 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+
+template<typename T>
+class BinImage {
+private:
+    std::vector<T> _matrix;
+    int _height, _width;
+public:
+    BinImage(int height, int width) : _height(height), _width(width) {
+        _matrix.resize(height * width);
+        std::fill_n(begin(), height * width, 0);
+    }
+
+    BinImage(const BinImage& image) = default;
+    ~BinImage() = default;
+
+    int get_height() const { return _height; }
+    int get_width() const { return _width; }
+
+    auto begin() { return _matrix.begin(); }
+    auto end() { return _matrix.end(); }
+
+    static void rprint_standart(const BinImage<T>& image, int x = 0, int y = 0) {
+        if (x < image._height) {
+            if (y < image._width) {
+                if (image._matrix[x*image._width +y] == 1) { std::cout << "1"; }
+                else { std::cout << "."; }
+                rprint_standart(image, x, ++y);
+            }
+            else {
+                std::cout << "\n";
+                rprint_standart(image, ++x, 0);
+            }
+        }
+        else {
+            return;
+        }
+    }
+
+    BinImage<T> operator!() {
+        BinImage<T> res(_height, _width);
+
+        T val1;
+
+        for (auto& i : _matrix)
+            for (auto& j : i)
+                if (_matrix[i * _width + j] != 0) { val1 = _matrix[i * _width + j]; break; }
+
+        for (auto& i : _matrix) {
+            for (auto& j : i) {
+                if (_matrix[i * _width + j] == 0) { res._matrix[i * _width + j] = val1; }
+                else { res._matrix[i * _width + j] = 0; }
+                
+            }
+        } return res;
+    }
+
+    BinImage<T> operator+(const BinImage<T>& image) {
+        if (image._height != _height || image._width != _width) throw std::out_of_range("the main parameters of the image are not equal!");
+        BinImage<T> new_image(_height, _width);
+        for (auto& i : new_image._matrix) {
+            for (auto& j : i) {
+                new_image._matrix[i * _width + j] = _matrix[i * _width + j] + image._matrix[i * _width + j];
+            }
+        } return new_image;
+    }
+    BinImage<T> operator*(const BinImage<T>& image) {
+        if (image._height != _height || image._width != _width) throw std::out_of_range("the main parameters of the image are not equal!");
+        BinImage<T> new_image(_height, _width);
+        for (auto& i : new_image._matrix) {
+            for (auto& j : i) {
+                new_image._matrix[i * _width + j] = _matrix[i * _width + j] * image._matrix[i * _width + j];
+            }
+        } return new_image;
+    }
+
+    BinImage<T> operator+(T value) {
+        BinImage<T> new_image(_height, _width);
+
+        for (auto& i : new_image._matrix) {
+            for (auto& j : i) {
+                new_image._matrix[i * _width + j] = value + _matrix[i * _width + j];
+            }
+        }
+        return new_image;
+    }
+    BinImage<T> operator*(T value) {
+        BinImage<T> new_image(_height, _width);
+        for (auto& i : new_image._matrix) {
+            for (auto& j : i) {
+                new_image._matrix[i * _width + j] = value * _matrix[i * _width + j];
+            }    
+        }
+        return new_image;
+    }
+
+    BinImage<T>& operator=(const BinImage<T>& image) = default;
+
+    double fill_rate() const {
+        if (_height == 0 || _width == 0) throw std::out_of_range("error! height or width = 0!");
+        int count = 0, size = _height * _width;
+        for (auto& i : _matrix) {
+            for (auto& j : i) {
+                if (_matrix[i * _width + j])
+                    ++count;
+            }
+        } return (double)count / (double)size;
+    }
+
+ /*   friend std::ostream& operator<<(std::ostream & os, const BinImage<T>& image) {
+        if (image._height == 0 || image._width == 0 || image._height < 0 || image._width < 0) throw std::out_of_range("invalid height or width");
+        for (auto& i : image._matrix) {
+            for (auto& j : i) {
+                os << image._matrix[i * image._width + j] << " ";
+            }
+            os << std::endl;
+        } return os;
+    }*/
+
+    T operator()(int x, int y) const {
+        if (x < 0 || x >= _height || y < 0 || y >= _width) throw std::out_of_range("error!");
+        return _matrix[x * _width + y];
+    }
+
+    T& operator()(int x, int y) {
+        if (x < 0 || x >= _height || y < 0 || y >= _width) throw std::out_of_range("error!");
+        return _matrix[x * _width + y];
+    }
+    static void rdraw_rectangle(BinImage<T> image, int x_1, int y_1, int x_2, int y_2, int i = 0, int j = 0, T value = 1) {
+        if (i <= x_2 && i < image._height) {
+            if (j <= y_2 && j < image._width) {
+                image._matrix[i * image._width + j] = value;
+                rdraw_rectangle(image, x_1, y_1, x_2, y_2, i, ++j, value);
+            }
+            else {
+                rdraw_rectangle(image, x_1, y_1, x_2, y_2, ++i, y_1, value);
+            }
+        }
+    }
+};
---
+++ laba3/Laba2_4.cpp
@@ -0,0 +1,348 @@
+﻿// л/р номер 2 вариант 6 (2)
+// Панюшкин Андрей Михайлович
+#include <iostream>
+#include "BinImage.h"
+#include "get_key.h"
+
+
+void text_for_menu() {
+    std::cout << "[1] Drawing of rectangle"
+        << std::endl << "[2] Invertion image (!)"
+        << std::endl << "[3] Output image"
+        << std::endl << "[4] Adding another image to the image (+)" // Добавление другого изображения к изображению (+)
+        << std::endl << "[5] Intersection of two images (*)" // Пересечение двух изображений (*)
+        << std::endl << "[6] Adding an image to a Boolean value" // Умножение изображения на булевое значение
+        << std::endl << "[7] Multiplying an image by a Boolean value" // Сложение изображения на булевое значение
+        << std::endl << "[8] Fill ratio"
+        << std::endl << "[Esc] Back"
+        << std::endl << "-->";
+}
+
+
+template<typename T>
+void menu(BinImage<T> image_1, BinImage<T> image_2) {
+    system("cls");
+
+    std::cout << "[1] First image" << std::endl << "[2] Second image" << std::endl << "[Backspace] Exit" << std::endl << "--> ";
+    while (true) {
+        int x_1, y_1, x_2, y_2;
+        T value;
+        int command_key = get_key2();
+        if (command_key == 8) {
+            system("cls");
+            std::cout << "Bye... waiting for you again...)";
+            break;
+        }
+        switch (command_key) {
+            // первое изображение 
+        case '1':
+        {
+            system("cls");
+            text_for_menu();
+            while (true) {
+                int commandKey = get_key();
+                if (commandKey == 27) {
+                    system("cls");
+                    std::cout << "[1] First image" << std::endl << "[2] Second image" << std::endl << "[Backspace] Exit" << std::endl << "--> "; break;
+                }
+                switch (commandKey) {
+                case '1':
+                    system("cls");
+                    try {
+                        std::cout << "First point.." << std::endl << "X--> ";
+                        std::cin >> x_1;
+                        std::cout << "Y--> ";
+                        std::cin >> y_1;
+                        std::cout << "Second point.." << std::endl << "X--> ";
+                        std::cin >> x_2;
+                        std::cout << "Y--> ";
+                        std::cin >> y_2;
+                        std::cout << "Value--> ";
+                        std::cin >> value;
+                        if (x_1 < 0 || y_1 < 0) {
+                            image_1.rdraw_rectangle(image_1, x_1, y_1, x_2, y_2, 0, 0, value);
+                        }
+                        else {
+                            image_1.rdraw_rectangle(image_1, x_1, y_1, x_2, y_2, x_1, y_1, value);
+                        }
+                        std::cout << "Okey.." << std::endl;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "[0] Back--> ";
+                    break;
+                case '2':
+                    system("cls");
+                    image_1 = !image_1;
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case '3':
+                    system("cls");
+                    //std::cout << image_1 << std::endl;
+                    std::cout << "[0] Back--> ";
+                    break;
+                case '4':
+                    system("cls");
+                    try {
+                        image_1 = image_1 + image_2;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+
+                case '5':
+                    system("cls");
+                    try {
+                        image_1 = image_1 * image_2;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case '6':
+                    system("cls");
+                    try {
+                        std::cout << "Enter a value--> "; std::cin >> value;
+                        image_1 = image_1 + value;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case '7':
+                    system("cls");
+                    try {
+                        bool value;
+                        std::cout << "Enter a boolean value--> "; std::cin >> value;
+                        image_1 = image_1 * value;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case '8':
+                    system("cls");
+                    try {
+                        std::cout << "Fill rate = " << image_1.fill_rate();
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << std::endl << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case 0:
+                    system("cls");
+                    system("color 7");
+                    text_for_menu();
+                    break;
+                default:
+                    system("cls");
+                    std::cout << "Error!" << std::endl;
+                    break;
+                }
+            }
+            break;
+        }
+        // второе изображение
+        case '2':
+        {
+            system("cls");
+            text_for_menu();
+            while (true) {
+                int command_key = get_key();
+                if (command_key == 27) {
+                    system("cls");
+                    std::cout << "[1] First image" << std::endl << "[2] Second image" << std::endl << "[Backspace] Exit" << std::endl << "--> "; break;
+                }
+                switch (command_key) {
+                case '1':
+                    system("cls");
+                    try {
+                        std::cout << "First point.." << std::endl << "X--> ";
+                        std::cin >> x_1;
+                        std::cout << "Y--> ";
+                        std::cin >> y_1;
+                        std::cout << "Second point.." << std::endl << "X--> ";
+                        std::cin >> x_2;
+                        std::cout << "Y--> ";
+                        std::cin >> y_2;
+                        std::cout << "Value--> ";
+                        std::cin >> value;
+                        if (x_1 < 0 || y_1 < 0) {
+                            image_1.rdraw_rectangle(image_2, x_1, y_1, x_2, y_2, 0, 0, value);
+                        }
+                        else {
+                            image_1.rdraw_rectangle(image_2, x_1, y_1, x_2, y_2, x_1, y_1, value);
+                        }
+                        std::cout << "Okey.." << std::endl;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "[0] Back--> ";
+                    break;
+                case '2':
+                    system("cls");
+                    image_2 = !image_2;
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case '3':
+                    system("cls");
+                    //std::cout << image_2 << std::endl;
+                    std::cout << "[0] Back--> ";
+                    break;
+                case '4':
+                    system("cls");
+                    try {
+                        image_2 = image_2 + image_1;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case '5':
+                    system("cls");
+                    try {
+                        image_2 = image_2 * image_1;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case '6':
+                    system("cls");
+                    try {
+                        std::cout << "Enter a value--> "; std::cin >> value;
+                        image_2 = image_2 + value;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case '7':
+                    system("cls");
+                    try {
+                        std::cout << "Enter a value--> "; std::cin >> value;
+                        image_2 = image_2 * value;
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case '8':
+                    system("cls");
+                    try {
+                        std::cout << "Fill rate = " << image_2.fill_rate();
+                    }
+                    catch (const std::out_of_range& err) {
+                        std::cerr << err.what() << std::endl;
+                    }
+                    std::cout << std::endl << "Okey.." << std::endl << "[0] Back--> ";
+                    break;
+                case 0:
+                    system("cls");
+                    system("color 7");
+                    text_for_menu();
+                    break;
+                default:
+                    system("cls");
+                    std::cout << "Error!" << std::endl;
+                    break;
+                }
+            }
+            break;
+        }
+        case 0:
+            system("cls");
+            std::cout << "[1] First image" << std::endl << "[2] Second image" << std::endl << "[Backspace] Exit" << std::endl << "--> "; break;
+        default:
+            system("cls");
+            std::cout << "Error!" << std::endl;
+            break;
+        }
+    }
+}
+
+int create_data_type() {
+    int height, width;
+
+    while (true) {
+        std::cout << "Please enter the size picture.." << std::endl << "Height--> "; std::cin >> height;
+        std::cout << "Width--> "; std::cin >> width;
+        if (width < 0 || height < 0) {
+            std::cout << "Try again! ";
+        }
+        else {
+            std::cout << "Okey.." << std::endl;
+            break;
+        }
+    }
+    system("cls");
+    std::cout << "[1] bool" << std::endl << "[2] char" << std::endl << "[3] short" << std::endl << "[4] float" << std::endl << "-->";
+    while (true) {
+        int command_key = get_key();
+        if (command_key == 8) {
+            system("cls");
+            std::cout << "Bye... waiting for you again...)";
+            break;
+        }
+        switch (command_key) {
+            case '1':
+            {
+                system("cls");
+                BinImage<bool> image_1(height, width), image_2(height, width);
+                menu(image_1, image_2);
+                break;
+            }
+            case '2':
+            {
+                system("cls");
+                BinImage<char> image_1(height, width), image_2(height, width);
+                menu(image_1, image_2);
+                break;
+            }
+            case '3':
+            {
+                system("cls");
+                BinImage<short> image_1(height, width), image_2(height, width);
+                menu(image_1, image_2);
+                break;
+            }
+            case '4':
+            {
+                system("cls");
+                BinImage<float> image_1(height, width), image_2(height, width);
+                menu(image_1, image_2);
+                break;
+            }
+            case 0:
+            {
+                system("cls");
+                std::cout << "[1] bool" << std::endl << "[2] char" << std::endl << "[3] short" << std::endl << "[4] float" << std::endl << "-->";
+                break;
+            }
+            default:
+            {
+                system("cls");
+                std::cout << "error!" << std::endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    create_data_type();
+    return 0;
+}

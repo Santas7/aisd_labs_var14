a5b804239ac3b311afb86d45aafd8bd647d8c055
Andrey_Panyshkin <86359412+Santas7@users.noreply.github.com>
2022-12-11 13:54:00 +0400

    Update Image.h

--- laba3/Image.h
+++ laba3/Image.h
@@ -1,20 +1,20 @@
-﻿#pragma once
+#pragma once
 #include <iostream>
 #include <vector>
 
 template<typename T>
-class BinImage {
+class Image {
 private:
     std::vector<T> _matrix;
     int _height, _width;
 public:
-    BinImage(int height, int width) : _height(height), _width(width) {
+    Image(int height, int width) : _height(height), _width(width) {
         _matrix.resize(height * width);
         std::fill_n(begin(), height * width, 0);
     }
 
-    BinImage(const BinImage& image) = default;
-    ~BinImage() = default;
+    Image(const Image& image) = default;
+    ~Image() = default;
 
     int get_height() const { return _height; }
     int get_width() const { return _width; }
@@ -22,7 +22,7 @@ public:
     auto begin() { return _matrix.begin(); }
     auto end() { return _matrix.end(); }
 
-    static void rprint_standart(const BinImage<T>& image, int x = 0, int y = 0) {
+    static void rprint_standart(const Image<T>& image, int x = 0, int y = 0) {
         if (x < image._height) {
             if (y < image._width) {
                 if (image._matrix[x*image._width +y] == 1) { std::cout << "1"; }
@@ -39,8 +39,8 @@ public:
         }
     }
 
-    BinImage<T> operator!() {
-        BinImage<T> res(_height, _width);
+    Image<T> operator!() {
+        Image<T> res(_height, _width);
 
         T val1;
 
@@ -57,18 +57,18 @@ public:
         } return res;
     }
 
-    BinImage<T> operator+(const BinImage<T>& image) {
+    Image<T> operator+(const Image<T>& image) {
         if (image._height != _height || image._width != _width) throw std::out_of_range("the main parameters of the image are not equal!");
-        BinImage<T> new_image(_height, _width);
+        Image<T> new_image(_height, _width);
         for (auto& i : new_image._matrix) {
             for (auto& j : i) {
                 new_image._matrix[i * _width + j] = _matrix[i * _width + j] + image._matrix[i * _width + j];
             }
         } return new_image;
     }
-    BinImage<T> operator*(const BinImage<T>& image) {
+    Image<T> operator*(const Image<T>& image) {
         if (image._height != _height || image._width != _width) throw std::out_of_range("the main parameters of the image are not equal!");
-        BinImage<T> new_image(_height, _width);
+        Image<T> new_image(_height, _width);
         for (auto& i : new_image._matrix) {
             for (auto& j : i) {
                 new_image._matrix[i * _width + j] = _matrix[i * _width + j] * image._matrix[i * _width + j];
@@ -76,8 +76,8 @@ public:
         } return new_image;
     }
 
-    BinImage<T> operator+(T value) {
-        BinImage<T> new_image(_height, _width);
+    Image<T> operator+(T value) {
+        Image<T> new_image(_height, _width);
 
         for (auto& i : new_image._matrix) {
             for (auto& j : i) {
@@ -86,8 +86,8 @@ public:
         }
         return new_image;
     }
-    BinImage<T> operator*(T value) {
-        BinImage<T> new_image(_height, _width);
+    Image<T> operator*(T value) {
+        Image<T> new_image(_height, _width);
         for (auto& i : new_image._matrix) {
             for (auto& j : i) {
                 new_image._matrix[i * _width + j] = value * _matrix[i * _width + j];
@@ -96,7 +96,7 @@ public:
         return new_image;
     }
 
-    BinImage<T>& operator=(const BinImage<T>& image) = default;
+    Image<T>& operator=(const Image<T>& image) = default;
 
     double fill_rate() const {
         if (_height == 0 || _width == 0) throw std::out_of_range("error! height or width = 0!");
@@ -109,7 +109,7 @@ public:
         } return (double)count / (double)size;
     }
 
- /*   friend std::ostream& operator<<(std::ostream & os, const BinImage<T>& image) {
+    friend std::ostream& operator<<(std::ostream & os, const Image<T>& image) {
         if (image._height == 0 || image._width == 0 || image._height < 0 || image._width < 0) throw std::out_of_range("invalid height or width");
         for (auto& i : image._matrix) {
             for (auto& j : i) {
@@ -117,7 +117,7 @@ public:
             }
             os << std::endl;
         } return os;
-    }*/
+    }
 
     T operator()(int x, int y) const {
         if (x < 0 || x >= _height || y < 0 || y >= _width) throw std::out_of_range("error!");
@@ -128,7 +128,8 @@ public:
         if (x < 0 || x >= _height || y < 0 || y >= _width) throw std::out_of_range("error!");
         return _matrix[x * _width + y];
     }
-    static void rdraw_rectangle(BinImage<T> image, int x_1, int y_1, int x_2, int y_2, int i = 0, int j = 0, T value = 1) {
+    
+    static void rdraw_rectangle(Image<T> image, int x_1, int y_1, int x_2, int y_2, int i = 0, int j = 0, T value = 1) {
         if (i <= x_2 && i < image._height) {
             if (j <= y_2 && j < image._width) {
                 image._matrix[i * image._width + j] = value;
